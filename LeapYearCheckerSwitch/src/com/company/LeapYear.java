c7539188432b4f91bab886ee8d86d687e6bb6b24
tanmaythaware <85193272+tanmaythaware@users.noreply.github.com>
2021-06-09 00:56:43 +0530

    Add files via upload

---
+++ LeapYearCheckerSwitch/src/com/company/LeapYear.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class LeapYear {
+
+    public static boolean isLeapYear(int year){
+        if(year < 1000 || year > 9999){
+            return false;
+        } else if ( year % 4 == 0 && year % 100 != 0){
+            return true;
+        } else if (year % 400 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+}
